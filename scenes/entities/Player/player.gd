d42bb0bafe259016466be2c6fd82b94b6b16dd97
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-09-29 18:00:25 +0700

    Update player.gd
    - Add the flipping function for left and right movement
    - Adjust the old code to remove redundancy

--- scenes/entities/Player/player.gd
+++ scenes/entities/Player/player.gd
@@ -1,7 +1,7 @@
 class_name Player extends CharacterBody2D
 
 @onready var animated_sprites : AnimatedSprite2D = $AnimatedSprite2D
-var move_speed : float = 400.0
+var move_speed : float = 300.0
 var current_direction = 'down' # Store the last known direction for idle animation
 
 # Called when the node enters the scene tree for the first time
@@ -24,10 +24,16 @@ func _physics_process(delta):
 	var direction : Vector2 = Vector2.ZERO
 	
 	direction.x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
+	if direction.x < 0:
+		animated_sprites.flip_h = true
+	else:
+		animated_sprites.flip_h = false
+	
 	direction.y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
 	
 	if direction.length() > 0:
 		direction = direction.normalized()
+	
 	
 	velocity = direction * move_speed
 	
@@ -36,13 +42,10 @@ func _physics_process(delta):
 	if velocity == Vector2.ZERO:
 		animated_sprites.play("idle")
 	else:
-		if direction.x != 0:
-			if direction.y != 0: 
-				animated_sprites.play("move_around")
-			else:
-				animated_sprites.play("vertical_move")
-		elif direction.y != 0:
+		if velocity.y != 0:
 			animated_sprites.play("move_around")
+		elif velocity.x != 0:
+			animated_sprites.play("vertical_move")
 		
 	
 	
