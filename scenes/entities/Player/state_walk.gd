9798e1f1d487322e53745f29870fe44823bf059f
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-10-02 23:47:34 +0700

    Update and Create state for walking
    - Create a state for walking

---
+++ scenes/entities/Player/state_walk.gd
@@ -0,0 +1,33 @@
+class_name State_Walk extends State
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+## What happens when the player exit this State?
+func Enter() -> void:
+	pass
+
+
+## What happens when the player exits this State?
+func Exit() -> void:
+	pass
+
+
+## What happens during the _process update in this State?
+func Process( _delta : float ) -> State:
+	return null
+
+
+
+## What happens during the _process update in this State?
+func Physics( _delta : float ) -> State:
+	return null
+
+
+## What happens during the _process update in this State?
+func HandleInput( _event: InputEvent ) -> State:
+	return null
