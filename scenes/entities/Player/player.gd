63fd07ff4d6544dceef299679a65422e8d79fb53
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-10-02 23:44:59 +0700

    Update player.gd and player.tscn
    - Create function for connecting with the state machine and player state
    - Update the player scene

--- scenes/entities/Player/player.gd
+++ scenes/entities/Player/player.gd
@@ -1,47 +1,70 @@
 class_name Player extends CharacterBody2D
 
+var move_speed : float = 200.0
+var current_direction : Vector2 = Vector2.ZERO # Store the last known direction for idle animation
+var direction : Vector2 = Vector2.ZERO # Declare the initial direction variable
+
 @onready var animated_sprites : AnimatedSprite2D = $AnimatedSprite2D
-var move_speed : float = 300.0
-var current_direction = 'down' # Store the last known direction for idle animation
+@onready var state_machine : PlayerStateMachine = $StateMachine
+
+
+
 
 # Called when the node enters the scene tree for the first time
 func _ready():
+	state_machine.Initialize( self )
 	pass # Replace with function body
 
-func _physics_process(delta):
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process( delta ):
 	
-	## Declare Variable for Direction Vector
-	var direction : Vector2 = Vector2.ZERO
 	
 	### Receive inputs from the player WASD buttons for movement
 	direction.x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
 	direction.y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
 	
-	### Normalized Diagonal Direction
-	if direction.length() > 0:
-		direction = direction.normalized()
+	### Normalized Diagonal Direction and Velocity calculation
+	velocity = direction.normalized() * move_speed if direction != Vector2.ZERO else Vector2.ZERO
 	
-	### Velocity calculation
-	velocity = direction * move_speed
+	pass
+
+
+
+func _physics_process( delta ):
+	SetDirection()
+	UpdateAnimatedSprite( AnimatedDirection() )
+	move_and_slide()
+
+
+
+func SetDirection() -> bool:
 	
 	## Flipping Mechanics
 	if velocity.x < 0:
 		animated_sprites.flip_h = true
+		return true
 	elif velocity.x > 0:
 		animated_sprites.flip_h = false
+		return false
 	else:
-		pass
-	
-	move_and_slide()
-	
-	## AnimatedSprites2D
+		return false
+
+
+
+func UpdateAnimatedSprite( state : String ) -> void:
+	animated_sprites.play( AnimatedDirection() + "_" + "move" ) if state != "idle" else animated_sprites.play( state )
+
+
+
+func AnimatedDirection() -> String:
 	if velocity == Vector2.ZERO:
-		animated_sprites.play("idle")
+		return "idle"
 	else:
 		if velocity.y != 0:
-			animated_sprites.play("move_around")
+			return "all_form"
 		elif velocity.x != 0:
-			animated_sprites.play("vertical_move")
-		
-	
-	
+			return "vertical"
+		else:
+			return "idle"
