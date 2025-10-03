ccf0aee3f161d81fbbbf0a0aef4cc65375b62929
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-10-03 23:03:48 +0700

    Update state_idle.gd
    - Connect to walk state

--- scenes/entities/Player/state_idle.gd
+++ scenes/entities/Player/state_idle.gd
@@ -1,11 +1,11 @@
 class_name State_Idle extends State
 
-
+@onready var walk : State = $'../Walk'
 
 
 ## What happens when the player exit this State?
 func Enter() -> void:
-	#player.
+	player.UpdateAnimatedSprite( "idle" )
 	pass
 
 
@@ -16,8 +16,10 @@ func Exit() -> void:
 
 ## What happens during the _process update in this State?
 func Process( _delta : float ) -> State:
+	if player.direction != Vector2.ZERO:
+		return walk
+	player.velocity = Vector2.ZERO
 	return null
-
 
 
 ## What happens during the _process update in this State?
