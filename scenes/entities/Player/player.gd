7b638b1c4314e4bc66b8cc6340b7c96f13c8c173
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-10-06 01:24:29 +0700

    Summary
    - player.gd :
       - Change the AnimatedDirection to AnimSpritesDirection and its properties
       - Make the move_speed a global constant
       - Use direction as constraint instead of velocity
    - player.tscn : trial & error with Attacking Animation

--- scenes/entities/Player/player.gd
+++ scenes/entities/Player/player.gd
@@ -1,7 +1,6 @@
 class_name Player extends CharacterBody2D
 
-var move_speed : float = 200.0
-var current_direction : Vector2 = Vector2.ZERO # Store the last known direction for idle animation
+const  MOVE_SPEED : float = 200.0
 var direction : Vector2 = Vector2.ZERO # Declare the initial direction variable
 
 @onready var animated_sprites : AnimatedSprite2D = $AnimatedSprite2D
@@ -35,7 +34,7 @@ func _process( delta ):
 		direction.y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
 		
 		### Normalized Diagonal Direction and Velocity calculation
-		velocity = direction.normalized() * move_speed if direction != Vector2.ZERO else Vector2.ZERO
+		velocity = direction.normalized() * MOVE_SPEED if direction != Vector2.ZERO else Vector2.ZERO
 	
 	pass
 
@@ -43,37 +42,40 @@ func _process( delta ):
 
 func _physics_process( delta ):
 	SetDirection()
-	UpdateAnimatedSprite( AnimatedDirection() )
+	UpdateAnimatedSprite( AnimSpritesDirection() )
 	move_and_slide()
 
 
 
 func SetDirection() -> bool:
 	## Flipping Mechanics
-	if velocity.x < 0:
+	if direction.x < 0:
 		animated_sprites.flip_h = true
 		return true
-	elif velocity.x > 0:
+	elif direction.x > 0:
 		animated_sprites.flip_h = false
 		return false
 	else:
 		return false
-
+ 
 
 
 func UpdateAnimatedSprite( state : String ) -> void:
-	animated_sprites.play( AnimatedDirection() + "_" + "move" ) if state != "idle" else animated_sprites.play( state )
+	if state != "idle":
+		animated_sprites.play( state ) 
+	else:
+		animated_sprites.play( "idle" )
 
 
 
-func AnimatedDirection() -> String:
+func AnimSpritesDirection() -> String:
 	## Animated Sprites
-	if velocity == Vector2.ZERO:
+	if direction == Vector2.ZERO:
 		return "idle"
 	else:
-		if velocity.y != 0:
-			return "all_form"
-		elif velocity.x != 0:
-			return "vertical"
+		if direction.y != 0:
+			return "all_form_move"
+		elif direction.x != 0:
+			return "horizontal_move"
 		else:
 			return "idle"
