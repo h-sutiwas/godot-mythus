c0148bf1bb6862e7b0836ea7214b902a89130a60
BusGasamawat <gasamawat.jon@gmail.com>
2025-10-14 03:51:18 +0700

    Update medjed warning & code

--- scenes/entities/Enemy/medjed.gd
+++ scenes/entities/Enemy/medjed.gd
@@ -2,11 +2,12 @@ class_name Medjed extends CharacterBody2D
 @onready var animated_sprites : AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: CharacterBody2D = get_tree().get_first_node_in_group("Player")
 
-const dist_before_attack = 150
+const dist_before_attack = 300
 const dist_toscreen_x = 500
 const dist_toscreen_y = 300
 var isAttacking = false
 var isVisible = false
+var isPlayerNear = false
 var pos : Vector2
 var spawn_pos : Vector2
 var player_move : bool
@@ -21,19 +22,41 @@ func _ready():
 	pos = global_position
 	player_pos = player.global_position
 	player_oldpos = player.global_position
+	$warning.play("warning")
 	if isAttacking == false:
 		$Laser.visible = false
+		$warning.visible = false
 
 
 
 
 func _physics_process(delta):
 	player_pos = player.global_position
+	
+	
 	#check if player move
 	if player_pos != player_oldpos:
 		player_move = true
 	else:
 		player_move = false
+		
+	#when player is near
+	if position.distance_to(player.global_position) < dist_before_attack:
+		isAttacking = true
+		$warning.visible = true
+		
+		#too many play same time?????
+		#$"sfx_medjed_warning".play()
+		#Timer #count ซัก 5 วิ
+		
+	#when player is not near
+	if position.distance_to(player.global_position) > dist_before_attack:
+		$warning.visible = false
+		
+		
+		
+		
+		
 		
 	#medjed turn left/right toward player
 	if player_pos.x > pos.x:
