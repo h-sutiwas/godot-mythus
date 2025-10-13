b0d1808ef87ce1ca257ab04fbc9de89f8113ce4a
BusGasamawat <gasamawat.jon@gmail.com>
2025-10-13 15:56:55 +0700

    Update Medjed atk anim

--- scenes/entities/Enemy/medjed.gd
+++ scenes/entities/Enemy/medjed.gd
@@ -5,14 +5,35 @@ class_name Medjed extends CharacterBody2D
 var isAttacking = false
 var pos : Vector2
 var spawn_pos : Vector2
+var player_move : bool
+var player_pos : Vector2
+var player_oldpos : Vector2
 
 
 func _ready():
 	pos = global_position
+	player_pos = player.global_position
+	player_oldpos = player.global_position
 	if isAttacking == false:
 		$Laser.visible = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func _physics_process(delta: float) -> void:
+	player_pos = player.global_position
+	#check if player move
+	if player_pos != player_oldpos:
+		player_move = true
+	else:
+		player_move = false
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	player_oldpos = player_pos
