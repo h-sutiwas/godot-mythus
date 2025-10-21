b3aac4c6aa2e357d2ef541b28da86fdc299ea5c0
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-10-21 20:02:07 +0700

    Refactorring enemyRevised.gd

--- scenes/entities/EnemyRevised/enemyRevised.gd
+++ scenes/entities/EnemyRevised/enemyRevised.gd
@@ -3,50 +3,47 @@ class_name EnemyRevised
 
 signal enemy_damaged( hurt_box : HurtBox )
 
-@export var invulnerable : bool = false
-@export var hp : int = 24.0 # Will change to const for this type of enemy
-@export var damage: int = 6
-#@export var position: Vector2 = self.global_position
-@onready var isAttacking : bool = false
 
-@export var SPEED : float = 70.0
-@export var dist_before_attack : float = 18
-@export var dist_before_chase : float = 35
-
-@onready var animation_player : AnimationPlayer = $AnimationPlayer
 @onready var animated_sprites : AnimatedSprite2D = $AnimatedSprite2D
+@onready var hit_box : HitBox = $HitBox
+@onready var AtkBall : AnimatedSprite2D = $HitBox/AtkBall
+
 @onready var state_machine : EnemyStateMachine = $StateMachine
 
-@onready var player: CharacterBody2D = get_tree().get_first_node_in_group( "Player" )
-
-@onready var AtkBall : AnimatedSprite2D = $HitBox/AtkBall
-@onready var hit_box : HitBox = $HitBox
-
 var gp : Player
+@onready var player: Player
+
+@export var hp : int = 24.0
+@export var damage: int = 6
+@export var regular_speed : float = 70.0
+@export var chase_speed : float = 100
+
+@onready var isWander : bool = true
+@onready var isChasing : bool = false
+@onready var isAttacking : bool = false
+@export var invulnerable : bool = false
+
+@export var dist_before_attack : float = 120
+@export var dist_before_chase : float = 300
+
 
 # Called when the node enters the scene tree for the first time
 func _ready():
-	gp = GlobalPlayerManager.player
+	player = GlobalPlayerManager.player
+	state_machine.Initialize( self )
 	hit_box.Damaged.connect( _take_damage )
 	pass # Replace with function body
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process( delta ):
+	
 	pass
 
 
 
 func _physics_process( delta ):
 	move_and_slide()
-	
-	if velocity.length() > 0:
-		animated_sprites.play( "walk" )
-	
-	if velocity.x > 0:
-		animated_sprites.flip_h = true
-	else:
-		animated_sprites.flip_h = false
 
 
 
@@ -55,7 +52,6 @@ func _take_damage( hurt_box : HurtBox ) -> void:
 		return
 	
 	hp -= hurt_box.damage
-	
 	emit_signal( "damaged" )
 	
 	if hp > 0:
